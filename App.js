eae8256bb300bb6b9ffd8fa98cd69d3f7292c818
Steffen Sornpao <steffenssornpao@gmail.com>
2019-11-21 23:32:58 -0500

    replace stylesheets with styled-components

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import styled from 'styled-components'
+
+const Container = styled.View`
+align-items: center;
+background: white;
+flex: 1;
+justify-content: center;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <Text>This is a cocktail app</Text>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
